e62e4131c7977b7d965b1f05205e67033cbf0b7c
Sarah <s.f.alashban@gmail.com>
2019-12-23 20:49:29 +0300

    Third Task

---
+++ madlibs.py
@@ -0,0 +1,15 @@
+print("Mad libs where libs get mad.")
+print("Start below:")
+
+time = input("Enter a number between 1 and 12:")
+items = input("Enter a noun (plural):")
+name = input("Enter a name:")
+scream = input("Enter any sentence:")
+action = input ("Enter a verb:")
+
+print("The story goes...")
+
+print("It was", time, "o'clock when I heard a knock at the door.")
+print("I opened the door and there was a box full of", items, "with a note saying \"From Mr.", name.capitalize(),"\"")
+print("Just as I closed the door I heard a scream \"",scream.upper(),"\"")
+print("I froze in place and all I could do was", action)
